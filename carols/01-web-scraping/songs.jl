33fe95ef3713bce897bc40a374f9a83406b8280d
Tibor Udvari <tibor.udvari@icloud.com>
2018-12-15 16:51:33 +0100

    Organisation

--- carols/01-web-scraping/songs.jl
+++
@@ -1,4 +0,0 @@
-{"song": "No\u00ebl, No\u00ebl, mille \u00e9toiles dans le ciel\r"}
-{"song": "[Swae Lee & Gucci Mane]\r"}
-{"song": "No\u00ebl, No\u00ebl, mille \u00e9toiles dans le ciel\r"}
-{"song": "No\u00ebl, No\u00ebl, mille \u00e9toiles dans le ciel\r"}
